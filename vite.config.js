392325bc4f46d30d86ffe79085d800cbe3ae7c81
buildbysachin <buildbysachin@gmail.com>
2026-07-07 17:21:52 +0530

    5th commit

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base: "/E-commerce-Store/",
+  base: "/E-commerse-Store/",
 })
